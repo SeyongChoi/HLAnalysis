c3616e07f7b1283b0bb83c120bcb86536d69c600
SeyongChoi <choiseyong6014@gmail.com>
2025-11-07 17:10:23 +0900

    commit utils

---
+++ src/utils/mod.rs
@@ -0,0 +1,18 @@
+pub mod histogram;
+pub mod physchem;
+
+use pyo3::prelude::*;
+
+pub fn register(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    // Create submodule: utils.histogram
+    let submod = PyModule::new(py, "histogram")?;
+    histogram::histogram(py, submod)?; // attach its functions
+    m.add_submodule(submod)?;
+
+    // Create submodule: utils.physchem
+    let submod = PyModule::new(py, "physchem")?;
+    physchem::physchem(py, submod)?; // attach its functions
+    m.add_submodule(submod)?;
+    
+    Ok(())
+}
